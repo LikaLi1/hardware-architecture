548620809589f93d90aec7fbdb77879dde55b617
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-12-02 12:34:07 +0300

    pr2.cpp

--- pr2.cpp
+++ pr2.cpp
@@ -13,13 +13,15 @@ int main() {
     int cycle = 0;
     bool finished;
 
-    while (true) {
+    while (true) 
+    {
         cycle++;
         cout << "Cycle " << cycle << ":\n";
 
         finished = true; 
 
-        if (stageI1 >= 0 && stageI1 < STAGES) {
+        if (stageI1 >= 0 && stageI1 < STAGES) 
+        {
             cout << "  " << stageNames[stageI1] << ": I1 (X=A+B)\n";
             stageI1++;
             if (stageI1 >= STAGES) {
@@ -28,30 +30,34 @@ int main() {
             finished = false;
         }
 
-        if (stageI2 >= 0 && stageI2 < STAGES) {
-
-            if (stageI2 == 2 && stageI1 != -1 && stageI1 < STAGES - 1) {
-
+        if (stageI2 >= 0 && stageI2 < STAGES) 
+        {
+            if (stageI2 == 2 && stageI1 != -1 && stageI1 < STAGES - 1) 
+            {
                 cout << "  " << stageNames[stageI2] << ": I2 (waiting for I1)\n";
             }
             else {
                 cout << "  " << stageNames[stageI2] << ": I2 (Y= X*2)\n";
                 stageI2++;
-                if (stageI2 >= STAGES) {
+                if (stageI2 >= STAGES) 
+                {
                     stageI2 = -1;
                 }
                 finished = false;
             }
         }
 
-        if (stageI3 >= 0 && stageI3 < STAGES) {
-            if (stageI3 == 2 && stageI2 != -1 && stageI2 < STAGES - 1) {
+        if (stageI3 >= 0 && stageI3 < STAGES) 
+        {
+            if (stageI3 == 2 && stageI2 != -1 && stageI2 < STAGES - 1) 
+            {
                 cout << "  " << stageNames[stageI3] << ": I3 (waiting for I2)\n";
             }
             else {
                 cout << "  " << stageNames[stageI3] << ": I3 (Z=Y-3)\n";
                 stageI3++;
-                if (stageI3 >= STAGES) {
+                if (stageI3 >= STAGES) 
+                {
                     stageI3 = -1;
                 }
                 finished = false;
@@ -60,7 +66,8 @@ int main() {
 
         cout << "-----------------\n";
 
-        if (stageI1 == -1 && stageI2 == -1 && stageI3 == -1) {
+        if (stageI1 == -1 && stageI2 == -1 && stageI3 == -1) 
+        {
             break;
         }
     }
